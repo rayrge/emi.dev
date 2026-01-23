1987998de051e3fc27f938e86af11ffde0f4fecb
rayrge <124107237+rayrge@users.noreply.github.com>
2026-01-22 22:41:37 -0500

    does this work fr

--- ck+/script/save.js
+++ ck+/script/save.js
@@ -186,7 +186,7 @@ function readFile(file) {
 				}
 				box = pokemon;
 				deadBox = deadPokemon;
-				parseBadges((bytes[0x23e5] << 8) | bytes[0x23e6]);
+				parseBadges((bytes[0x23eb] << 8) | bytes[0x23ec]);
 				finishParse("Successfully parsed save!", pokemon, deadPokemon);
 			} catch (e) {
 				console.log(e);
