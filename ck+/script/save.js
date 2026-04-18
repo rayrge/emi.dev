44c287ce00602374bd6df88bb163efc5c8de0c78
rayrge <124107237+rayrge@users.noreply.github.com>
2026-04-18 00:50:09 -0400

    fix that part woops

--- ck+/script/save.js
+++ ck+/script/save.js
@@ -187,7 +187,7 @@ function readFile(file) {
 			try {
 				var pokemon = [];
 				var deadPokemon = [];
-				pokemon = pokemon.concat(readPokemonList(bytes, 0x2865, 6, 48));
+				pokemon = pokemon.concat(readPokemonList(bytes, 0x286B, 6, 48));
 				for (var i = 0; i < 16; i++) {
 					var l = readNewbox(bytes, 0x2f20 + i * 0x21, 0x4000, 0x6000);
 					if (i >= 12) {
